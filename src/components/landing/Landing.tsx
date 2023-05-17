407dbda68b7d736e2e873b16b9443682252773a2
Argiro <elto.82@gmai.com>
2023-05-17 18:00:44 -0500

    ld center

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,41 +1,41 @@
-import { Container, Grid, Box } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import video from "../../image/Diseño sin título_1.mp4";
 import { Link } from "react-router-dom";
 import s from "../landing/Landing.module.css";
 
 export const Landing = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", minHeight: "100vh" }}>
-      <Container>
-        <Grid container justifyContent="center">
-          <Grid item xs={12} md={8} lg={6}>
-            <video
-              id="video"
-              autoPlay
-              loop
-              muted
-              src={video}
-              style={{
-                width: "100%",
-                objectFit: "cover",
-                zIndex: "-1",
-              }}
-            ></video>
-            <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
-              <Link to="/home">
-                <strong>
-                  <button className={s.bt}>INGRESAR</button>
-                </strong>
-              </Link>
+    <div style={{ minHeight: "100vh" }}>
+      <video
+        id="video"
+        autoPlay
+        loop
+        muted
+        src={video}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: "-1",
+        }}
+      ></video>
+      <Container style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", minHeight: "100vh" }}>
+        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
+          <Link to="/home">
+            <strong>
+              <button className={s.bt}>INGRESAR</button>
+            </strong>
+          </Link>
 
-              <Link to="/about">
-                <strong>
-                  <button className={s.bt}>NOSOTROS</button>
-                </strong>
-              </Link>
-            </Box>
-          </Grid>
-        </Grid>
+          <Link to="/about">
+            <strong>
+              <button className={s.bt}>NOSOTROS</button>
+            </strong>
+          </Link>
+        </Box>
       </Container>
     </div>
   );
