237e4c798f6c911df2651a03d7ded108a6beb098
Argiro <elto.82@gmai.com>
2023-05-16 19:39:56 -0500

    getPropertyById2

--- src/reduxToolkit/apiSlice.ts
+++ src/reduxToolkit/apiSlice.ts
@@ -24,6 +24,15 @@ export const apiSlice = createApi({
     getPropertyById: builder.query<property, string>({
       queryFn: async (id) => {
         const response = await fetch(`/property/${id}`);
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+
+        const contentType = response.headers.get("Content-Type");
+        if (!contentType || !contentType.includes("application/json")) {
+          throw new Error("La respuesta no es un JSON válido");
+        }
+
         const data = await response.json();
         console.log("Response:", data); // Agrega el registro de consola aquí
         return data;
