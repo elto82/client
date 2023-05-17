f06023207167fba857fb4c17e6b8c149a8d287cc
Argiro <elto.82@gmai.com>
2023-05-17 18:03:28 -0500

    landing c

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,41 +1,47 @@
-import { Container, Box } from "@mui/material";
+import { Container, Grid, Box } from "@mui/material";
 import video from "../../image/Diseño sin título_1.mp4";
 import { Link } from "react-router-dom";
 import s from "../landing/Landing.module.css";
 
 export const Landing = () => {
   return (
-    <div style={{ minHeight: "100vh" }}>
-      <video
-        id="video"
-        autoPlay
-        loop
-        muted
-        src={video}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          zIndex: "-1",
-        }}
-      ></video>
-      <Container style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", minHeight: "100vh" }}>
-        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
-          <Link to="/home">
-            <strong>
-              <button className={s.bt}>INGRESAR</button>
-            </strong>
-          </Link>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Container>
+        <Grid container>
+          <Grid item xs={8} sm={8} md={6} lg={4}>
+            <video
+              id="video"
+              autoPlay
+              loop
+              muted
+              src={video}
+              style={{
+                position: "fixed",
+                width: "100%",
+                left: "50%",
+                top: "50%",
 
-          <Link to="/about">
-            <strong>
-              <button className={s.bt}>NOSOTROS</button>
-            </strong>
-          </Link>
-        </Box>
+                objectFit: "cover",
+                transform: "translate(-50%, -50%)",
+                zIndex: "-1",
+              }}
+            ></video>
+            <br />
+            <Box display="flex" justifyContent="center" alignContent="center" sx={{ ml: 80 }}>
+              <Link to="/home">
+                <strong>
+                  <button className={s.bt}>INGRESAR</button>
+                </strong>
+              </Link>
+
+              <Link to="/about">
+                <strong>
+                  <button className={s.bt}>NOSOTROS</button>
+                </strong>
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
