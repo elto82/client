4e47f68b72cbf72cf0c515b3a37dca06ad7e7d1b
Argiro <elto.82@gmai.com>
2023-05-20 08:09:53 -0500

    update url back

--- src/reduxToolkit/apiSlice.ts
+++ src/reduxToolkit/apiSlice.ts
@@ -5,7 +5,7 @@ import { Broker, CreateBrokerRequest } from "./brokerInterfaces";
 import { createUserRequest } from "./authentication";
 import { User } from "./userInterface";
 
-const API_URL = "http://localhost:3001";
+const API_URL = "api-proptech.up.railway.app";
 
 export const apiSlice = createApi({
   //metodos para enviar y recibr data de la ruta property
